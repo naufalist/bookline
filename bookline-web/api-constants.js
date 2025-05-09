1b5ff4d9a1e72ece669e0c82f4ab11dce0e031fd
Naufal <mnaufalwafi@gmail.com>
2025-05-09 15:49:41 +0700

    chore: update launch profile port and base API URL

--- bookline-web/api-constants.js
+++ bookline-web/api-constants.js
@@ -1,11 +1,16 @@
-const ENV = window.location.hostname.includes("localhost") ? "development" : "production";
+const isLocalhost = window.location.hostname.includes("localhost");
+const isFileProtocol = window.location.protocol === "file:";
+
+const ENV = (isLocalhost || isFileProtocol) ? "development" : "production";
+
+console.log("ENV:", ENV);
 
 const BASE_API_URL = ENV === "development"
     ? "https://localhost:44312"
-    : "https://api.azurewebsites.net";
+    : "https://bookline-api-d8dza8fchadmhze7.indonesiacentral-01.azurewebsites.net";
 
 const API_ENDPOINTS = {
-    appointmentsByDate: (date) => `${BASE_API_URL}/appointments/${date}`,
+    appointmentsByDate: (date) => `${BASE_API_URL}/api/appointments/${date}`,
     appointments: `${BASE_API_URL}/api/appointments`,
     customerRegister: `${BASE_API_URL}/api/customers/register`,
     dayConfigs: `${BASE_API_URL}/api/appointment-day-configs`
